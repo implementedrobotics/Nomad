2ae690de3a54903293f152129fd8fdf2808688dd
Quincy Jones <quincy@implementedrobotics.com>
2020-07-20 08:54:46 -0500

    Working on sim.

---
+++ Software/Simulation/Dart/src/NomadRobot.cpp
@@ -0,0 +1,120 @@
+/*
+ * NomadRobot.cpp
+ *
+ *  Created on: June 23, 2020
+ *      Author: Quincy Jones
+ *
+ * Copyright (c) <2020> <Quincy Jones - quincy@implementedrobotics.com/>
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// C System Files
+
+// C++ System Files
+
+// Third Party Includes
+#include <dart/utils/urdf/DartLoader.hpp>
+#include <dart/dynamics/DegreeOfFreedom.hpp>
+#include <dart/gui/osg/osg.hpp>
+
+// Project Include Files
+#include "../include/NomadRobot.h"
+
+NomadRobot::NomadRobot(const dart::simulation::WorldPtr world)
+    : world_(world)
+{
+    // Reset
+    Reset();
+}
+
+void NomadRobot::ProcessInputs()
+{
+
+}
+
+void NomadRobot::Run(double dt)
+{
+
+}
+
+void NomadRobot::SendOutputs()
+{
+}
+
+void NomadRobot::UpdateState()
+{
+
+}
+void NomadRobot::Reset()
+{
+
+}
+void NomadRobot::LoadFromURDF(const std::string &urdf)
+{
+    dart::utils::DartLoader loader;
+    robot_ = loader.parseSkeleton(urdf);
+
+    // Rename the floating base dofs
+    robot_->getDof(0)->setName("omega_x");
+    robot_->getDof(1)->setName("omega_y");
+    robot_->getDof(2)->setName("omega_z");
+    robot_->getDof(3)->setName("base_x");
+    robot_->getDof(4)->setName("base_y");
+    robot_->getDof(5)->setName("base_z");
+
+    // Set position limits enforcement
+    robot_->getJoint("j_kfe_FL")->setPositionLimitEnforced(true);
+    robot_->getJoint("j_kfe_FR")->setPositionLimitEnforced(true);
+    robot_->getJoint("j_kfe_RL")->setPositionLimitEnforced(true);
+    robot_->getJoint("j_kfe_RR")->setPositionLimitEnforced(true);
+
+    int i = 0;
+    for(auto dof:robot_->getDofs())
+    {
+        std::cout << "DOF: " << i++ << " : " << dof->getName() << std::endl;
+    }
+
+    // i = 0;
+    // for(auto dof:robot_->getBodyNodes())
+    // {
+    //     std::cout << "DOF: " << i++ << " : " << dof->getName() << std::endl;
+    // }
+
+    // Add to world
+    world_->addSkeleton(robot_);
+
+	std::cout << "Mass Matrix: " << robot_->getMassMatrix() << std::endl;
+    std::cout << "Mass: " << robot_->getMass() << std::endl;
+}
+
+void NomadRobot::SetInitialPose()
+{
+    robot_->getDof("base_z")->setPosition(0.2);
+    robot_->getDof("j_hfe_FL")->setPosition(-M_PI_2);
+    robot_->getDof("j_hfe_FR")->setPosition(M_PI_2);
+    robot_->getDof("j_hfe_RL")->setPosition(-M_PI_2);
+    robot_->getDof("j_hfe_RR")->setPosition(M_PI_2);
+
+    robot_->getDof("j_kfe_FL")->setPositionLimits(-2.2, 0.0);
+    robot_->getDof("j_kfe_FR")->setPositionLimits(0.0, 2.2);
+    robot_->getDof("j_kfe_RL")->setPositionLimits(-2.2, 0.0);
+    robot_->getDof("j_kfe_RR")->setPositionLimits(0.0, 2.2);
+
+    //robot_->getDof("j_kfe_RL")->setPosition(-2.2);
+    //robot_->getDof("j_kfe_FL")->setPosition(-2.2);
+    //robot_->getDof("j_kfe_RR")->setPosition(2.2);
+    //robot_->getDof("j_kfe_FR")->setPosition(2.2);
+}
---
+++ Software/Simulation/Dart/src/main.cpp
@@ -0,0 +1,140 @@
+
+#include <dart/dynamics/Skeleton.hpp>
+#include <dart/dynamics/DegreeOfFreedom.hpp>
+#include <dart/dynamics/WeldJoint.hpp>
+#include <dart/dynamics/SphereShape.hpp>
+#include <dart/simulation/World.hpp>
+#include <dart/utils/urdf/DartLoader.hpp>
+
+#include <dart/gui/osg/osg.hpp>
+#include <dart/gui/osg/TrackballManipulator.hpp>
+
+#include <vector>
+
+#include "../include/NomadRobot.h"
+
+ using namespace dart::dynamics;
+ using namespace dart::simulation;
+
+ WorldPtr g_world;
+
+ std::shared_ptr<NomadRobot> g_nomad;
+
+ // Robot Class
+ // Dynamic State Vector
+ // Floating Base Class
+
+class NomadSimWorldNode : public dart::gui::osg::RealTimeWorldNode
+{
+
+public:
+  explicit NomadSimWorldNode(WorldPtr world, const std::shared_ptr<NomadRobot> nomad)
+      : dart::gui::osg::RealTimeWorldNode(world), nomad_(nomad), world_(world)
+  {
+    // World gets saved as -> mWorld if ever needed.  Also ->getWorld()
+    Eigen::Isometry3d tf = Eigen::Isometry3d::Identity();
+  //  step_iter = 0;
+  }
+
+  void customPreStep()
+  {
+
+    // std::cout << leg->getJoint("leg_to_world")->getPosition(0) << std::endl;
+  //  if (step_iter < 5)
+  //    return;
+
+    // Reset Leg Controller
+   // nomad_->ProcessInputs();
+   // nomad_->Run(world_->getTimeStep());
+    //nomad_->SendOutputs();
+
+    // Post Setup
+  }
+
+  void customPostStep()
+  {
+   // nomad_->UpdateState();
+    step_iter++;
+  }
+
+protected:
+
+  std::shared_ptr<NomadRobot> nomad_;
+  WorldPtr world_;
+  int step_iter;
+};
+
+SkeletonPtr CreateGround()
+{
+  SkeletonPtr ground = Skeleton::create("ground");
+
+  // Give the ground a body
+  BodyNodePtr body = ground->createJointAndBodyNodePair<WeldJoint>(nullptr).second;
+  double thickness = 0.1;
+
+  ShapePtr groundShape = std::make_shared<BoxShape>(Eigen::Vector3d(4, 4, thickness));
+  auto shapeNode = body->createShapeNodeWith<VisualAspect, CollisionAspect, DynamicsAspect>(groundShape);
+
+  shapeNode->getVisualAspect()->setColor(dart::Color::LightGray());
+
+  Eigen::Isometry3d tf(Eigen::Isometry3d::Identity());
+  tf.translation() = Eigen::Vector3d(0, 0, -thickness / 2.0);
+  body->getParentJoint()->setTransformFromParentBodyNode(tf);
+
+  //shapeNode->getDynamicsAspect()->setFrictionCoeff(10.0);
+  //std::cout << "Mu: " << shapeNode->getDynamicsAspect()->getFrictionCoeff() << std::endl;
+
+  return ground;
+}
+
+int main(int argc, char *argv[])
+{
+
+  // Create dart simulation world
+  g_world = World::create();
+
+  // Update Gravity vector to Z down
+  Eigen::Vector3d gravity(0.0, 0.0, -9.81);
+  g_world->setGravity(gravity);
+
+  // Create and add ground to world
+  SkeletonPtr ground = CreateGround();
+  g_world->addSkeleton(ground);
+
+  g_nomad = std::make_shared<NomadRobot>(g_world);
+
+  std::string urdf = std::getenv("NOMAD_RESOURCE_PATH");
+  urdf.append("/Robot/Nomad.urdf");
+
+  g_nomad->LoadFromURDF(urdf);
+  g_nomad->SetInitialPose();
+
+  // Create osg world node
+  ::osg::ref_ptr<NomadSimWorldNode> node = new NomadSimWorldNode(g_world, g_nomad);
+
+   // Create osg Viewer
+   dart::gui::osg::Viewer viewer = dart::gui::osg::Viewer();
+   viewer.addWorldNode(node);
+
+   // Default with Headlights
+   viewer.switchHeadlights(false);
+
+   //Add Grid Attachment
+   ::osg::ref_ptr<dart::gui::osg::GridVisual> grid = new dart::gui::osg::GridVisual();
+
+   viewer.addAttachment(grid);
+
+   // Print out instructions for the viewer
+   std::cout << viewer.getInstructions() << std::endl;
+
+    viewer.setUpViewInWindow(0, 0, 1280, 1024);
+   viewer.getCameraManipulator()->setHomePosition(::osg::Vec3(0.0f, -5.0f, 0.0f),
+                                                  ::osg::Vec3(0.0f, 0.0f, 0.0f),
+                                                  ::osg::Vec3(0.0f, 0.707f, 0.707f), false);
+
+   viewer.setCameraManipulator(viewer.getCameraManipulator());
+
+   viewer.run();
+
+  return 0;
+}
