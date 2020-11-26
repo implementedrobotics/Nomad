d470755f0cc415637afe0a776e026f558ffb8883
Nomad <quincy@implementedrobotics.com>
2020-11-25 19:31:19 -0800

    Update python data containers for refactor

--- Hardware/Actuator/NomadBLDC/Tools/DataContainers.py
+++ Hardware/Actuator/NomadBLDC/Tools/DataContainers.py
@@ -60,39 +60,66 @@ class DeviceInfo:
 
 @dataclass
 class MotorConfig:
-    __packet : ClassVar[struct.Struct] = struct.Struct('<I10fiffi')
+    __packet : ClassVar[struct.Struct] = struct.Struct('<I8fiff6I2fi5I')
+    # Config Reg 1
     num_pole_pairs: int = None
-    continuous_current_max: float = None
-    continuous_current_tau: float = None
+    K_v: float = None
+    K_t: float = None
+    K_t_out: float = None
+    flux_linkage: float = None
     phase_resistance: float = None
     phase_inductance_d: float = None
     phase_inductance_q: float = None
-    K_v: float = None
-    flux_linkage: float = None
-    K_t: float = None
-    K_t_out: float = None
     gear_ratio: float = None
     phase_order: int = None
+
+    # Thermal Config Reg
+    continuous_current_max: float = None
+    continuous_current_tau: float = None
+    tcr_reserved_1: int = None
+    tcr_reserved_2: int = None
+    tcr_reserved_3: int = None
+    tcr_reserved_4: int = None
+    tcr_reserved_5: int = None
+    tcr_reserved_6: int = None
+
+    # Cablibration Config Reg
     calib_current: float = None
     calib_voltage: float = None
     calibrated: int = None
+    ccr_reserved_1: int = None
+    ccr_reserved_2: int = None
+    ccr_reserved_3: int = None
+    ccr_reserved_4: int = None
+    ccr_reserved_5: int = None
 
     def pack(self):
         return self.__packet.pack(self.num_pole_pairs,
-        self.continuous_current_max,
-        self.continuous_current_tau,
+        self.K_v,
+        self.K_t,
+        self.K_t_out,
+        self.flux_linkage,
         self.phase_resistance,
         self.phase_inductance_d,
         self.phase_inductance_q,
-        self.K_v,
-        self.flux_linkage,
-        self.K_t,
-        self.K_t_out,
         self.gear_ratio,
         self.phase_order,
+        self.continuous_current_max,
+        self.continuous_current_tau,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
         self.calib_current,
         self.calib_voltage,
-        self.calibrated)
+        self.calibrated,
+        0,
+        0,
+        0,
+        0,
+        0)
 
     @classmethod
     def unpack(cls, data):
@@ -103,43 +130,62 @@ class MotorConfig:
 @dataclass
 class ControllerConfig:
     #__fmt = "<16fI"
-    __packet : ClassVar[struct.Struct] = struct.Struct('<16fI')
+    __packet : ClassVar[struct.Struct] = struct.Struct('<7fI3I8f5I')
+    # Config Reg 1
     k_d: float = None
     k_q: float = None
     k_i_d: float = None
     k_i_q: float = None
-    alpha: float = None
-    overmodulation: float = None
-    velocity_limit: float = None
-    position_limit: float = None
-    torque_limit: float = None
-    current_limit: float = None
     current_bandwidth: float = None
+    overmodulation: float = None
+    pwm_freq: float = None
+    foc_ccl_divider: int = None
+    ccr1_reserved_1: int = None
+    ccr1_reserver_2: int = None
+    ccr1_reserver_3: int = None
+    
+    # Config Reg 2
     K_p_min: float = None
     K_p_max: float = None
     K_d_min: float = None
     K_d_max: float = None
-    pwm_freq: float = None
-    foc_ccl_divider: int = None
+    velocity_limit: float = None
+    position_limit: float = None
+    torque_limit: float = None
+    current_limit: float = None
+    ccr2_reserved_1: int = None
+    ccr2_reserver_2: int = None
+    ccr2_reserver_3: int = None
+    ccr2_reserver_4: int = None
+    ccr2_reserver_5: int = None
+
+
 
     def pack(self):
         return self.__packet.pack(self.k_d,
         self.k_q,
         self.k_i_d,
         self.k_i_q,
-        self.alpha,
-        self.overmodulation,
-        self.velocity_limit,
-        self.position_limit,
-        self.torque_limit,
-        self.current_limit,
         self.current_bandwidth,
+        self.overmodulation,
+        self.pwm_freq,
+        self.foc_ccl_divider,
+        0,
+        0,
+        0,
         self.K_p_min,
         self.K_p_max,
         self.K_d_min,
         self.K_d_max,
-        self.pwm_freq,
-        self.foc_ccl_divider)
+        self.velocity_limit,
+        self.position_limit,
+        self.torque_limit,
+        self.current_limit,
+        0,
+        0,
+        0,
+        0,
+        0)
 
     @classmethod
     def unpack(cls, data):
@@ -149,10 +195,14 @@ class ControllerConfig:
 @dataclass
 class EncoderConfig:
     #__fmt = "<ffi128b"
-    __packet : ClassVar[struct.Struct] = struct.Struct('<ffi128b')
+    __packet : ClassVar[struct.Struct] = struct.Struct('<ffi4I128b')
     offset_elec: float = None
     offset_mech: float = None
     cpr: int = None
+    ecr_reserved_1: int = None
+    ecr_reserved_2: int = None
+    ecr_reserved_3: int = None
+    ecr_reserved_4: int = None
     #direction: int = None
     offset_lut: List[int] = None
 
@@ -160,12 +210,16 @@ class EncoderConfig:
         return self.__packet.pack(self.offset_elec,
         self.offset_mech,
         self.cpr,
+        0,
+        0,
+        0,
+        0,
         *self.offset_lut)
 
     @classmethod
     def unpack(cls, data):
         unpacked = cls.__packet.unpack(data)
-        return EncoderConfig(unpacked[0], unpacked[1], unpacked[2], [*unpacked[3:]])
+        return EncoderConfig(unpacked[0], unpacked[1], unpacked[2], unpacked[3],unpacked[4],unpacked[5],unpacked[6],[*unpacked[7:]])
 
 @dataclass
 class MotorState:
@@ -177,12 +231,13 @@ class MotorState:
     #V_a: float = None
     #V_b: float = None
     #V_c: float = None
+    windings_temp: float = None
     theta_mech: float = None
     theta_mech_dot: float = None
     theta_mech_true: float = None
     theta_elec: float = None
     theta_elec_dot: float = None
-    windings_temp: float = None
+    
 
 
     def pack(self):
@@ -192,12 +247,12 @@ class MotorState:
         #self.V_a,
         #self.V_b,
         #self.V_c,
+        self.windings_temp,
         self.theta_mech,
         self.theta_mech_dot,
         self.theta_mech_true,
         self.theta_elec,
-        self.theta_elec_dot,
-        self.windings_temp)
+        self.theta_elec_dot)
 
     @classmethod
     def unpack(cls, data):
@@ -206,58 +261,66 @@ class MotorState:
 
 @dataclass
 class ControllerState:
-    __packet : ClassVar[struct.Struct] = struct.Struct('<23fI')
+    __packet : ClassVar[struct.Struct] = struct.Struct('<11fI12f')
     I_d: float = None
     I_q: float = None
-    I_d_ref: float = None
-    I_q_ref: float = None
-    d_int: float = None
-    q_int: float = None
     V_d: float = None
     V_q: float = None
+    d_int: float = None
+    q_int: float = None
+
+    dtc_A: float = None
+    dtc_B: float = None
+    dtc_C: float = None
+
+    I_rms: float = None
+    I_max: float = None
+
+    timeout: int = None
+
     V_d_ref: float = None
     V_q_ref: float = None
-    Voltage_bus: float = None
+
+    I_d_ref: float = None
+    I_q_ref: float = None
+
     Pos_ref: float = None
     Vel_ref: float = None
     K_p: float = None
     K_d: float = None
     T_ff: float = None
-    dtc_A: float = None
-    dtc_B: float = None
-    dtc_C: float = None
-    I_rms: float = None
-    I_max: float = None
+
+    Voltage_bus: float = None
     I_bus: float = None
     fet_temp: float = None
-    timeout: int = None
+    
 
     def pack(self):
         return self.__packet.pack(self.I_d,
         self.I_q,
-        self.I_c,
-        self.I_d_ref,
-        self.I_q_ref,
-        self.d_int,
-        self.q_int,
         self.V_d,
         self.V_q,
-        self.V_d_ref,
-        self.V_q_ref,
-        self.Voltage_bus,
-        self.Pos_ref,
-        self.Vel_ref,
-        self.K_p,
-        self.K_d,
-        self.T_ff,
+        self.d_int,
+        self.q_int,
         self.dtc_A,
         self.dtc_B,
         self.dtc_C,
         self.I_rms,
         self.I_max,
+        self.timeout,
+        self.V_d_ref,
+        self.V_q_ref,
+        self.I_d_ref,
+        self.I_q_ref,
+        self.Pos_ref,
+        self.Vel_ref,
+        self.K_p,
+        self.K_d,
+        self.T_ff,
+        self.Voltage_bus,
         self.I_bus,
         self.fet_temp,
-        self.timeout)
+        )
 
     @classmethod
     def unpack(cls, data):
