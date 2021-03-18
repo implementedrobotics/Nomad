beae5de4750384c3c82d5efac528f59dbdfcb2df
Quincy Jones <quincy@implementedrobotics.com>
2021-03-18 17:59:18 -0500

    PCAN Native.  Very ugly test code for now

--- Hardware/Actuator/NomadBLDC/Tools/CANTest/CAN/src/CANDevice.cpp
+++ Hardware/Actuator/NomadBLDC/Tools/CANTest/CAN/src/CANDevice.cpp
@@ -35,6 +35,7 @@
 
 #include <CAN/CANDevice.h>
 #include <linux/can/raw.h>
+#include <libpcanfd.h>
 
 
 CANDevice::CANDevice() :  socket_(-1)
@@ -43,47 +44,54 @@ CANDevice::CANDevice() :  socket_(-1)
 
 bool CANDevice::Open(const std::string &canid, int32_t timeout)
 {
-    rx_timeout_ = timeout;
-    int mtu, enable_canfd = 1;
-	struct sockaddr_can addr;
-	struct ifreq ifr;
+    // rx_timeout_ = timeout;
+    // int mtu, enable_canfd = 1;
+	// struct sockaddr_can addr;
+	// struct ifreq ifr;
 
-	if((socket_ = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
-	{
-		perror("Socket Create Error");
-		return false;
-	}
+	// if((socket_ = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
+	// {
+	// 	perror("Socket Create Error");
+	// 	return false;
+	// }
 
-	strcpy(ifr.ifr_name, canid.c_str());
-	ioctl(socket_, SIOCGIFINDEX, &ifr);
+	// strcpy(ifr.ifr_name, canid.c_str());
+	// ioctl(socket_, SIOCGIFINDEX, &ifr);
 
-	memset(&addr, 0, sizeof(addr));
-	addr.can_family = AF_CAN;
-	addr.can_ifindex = ifr.ifr_ifindex;
+	// memset(&addr, 0, sizeof(addr));
+	// addr.can_family = AF_CAN;
+	// addr.can_ifindex = ifr.ifr_ifindex;
 
-    if (ioctl(socket_, SIOCGIFMTU, &ifr) < 0)
-    {
-        perror("SIOCGIFMTU");
-        return false;
-    }
-    /* interface is ok - try to switch the socket into CAN FD mode */
-    if (setsockopt(socket_, SOL_CAN_RAW, CAN_RAW_FD_FRAMES,
-                   &enable_canfd, sizeof(enable_canfd)))
-    {
+    // if (ioctl(socket_, SIOCGIFMTU, &ifr) < 0)
+    // {
+    //     perror("SIOCGIFMTU");
+    //     return false;
+    // }
+    // /* interface is ok - try to switch the socket into CAN FD mode */
+    // if (setsockopt(socket_, SOL_CAN_RAW, CAN_RAW_FD_FRAMES,
+    //                &enable_canfd, sizeof(enable_canfd)))
+    // {
 
-        perror("here");
-        return false;
-    }
+    //     perror("here");
+    //     return false;
+    // }
 
-    // Set timeout for socket receive.  Notice some hanging when this is not set
-    struct timeval tv;
-    tv.tv_sec = 0;
-    tv.tv_usec = rx_timeout_ * 1000;
-    setsockopt(socket_, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(struct timeval));
+    // // Set timeout for socket receive.  Notice some hanging when this is not set
+    // struct timeval tv;
+    // tv.tv_sec = 0;
+    // tv.tv_usec = rx_timeout_ * 1000;
+    // setsockopt(socket_, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(struct timeval));
 
-    if (bind(socket_, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-	{
-		perror("BIND ERROR");
+    // if (bind(socket_, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	// {
+	// 	perror("BIND ERROR");
+	// 	return false;
+	// }
+    // return true;
+
+    socket_ = pcanfd_open(canid.c_str(), OFD_BITRATE | OFD_DBITRATE | OFD_CLOCKHZ | OFD_NONBLOCKING, 1000000,2000000, 80000000);
+	if (socket_ < 0) {
+		perror("pcanfd_open() failed");
 		return false;
 	}
     return true;
@@ -91,19 +99,34 @@ bool CANDevice::Open(const std::string &canid, int32_t timeout)
 
 bool CANDevice::Send(CAN_msg_t &msg)
 {
-    // TODO: Make sure socket is open and can device is valid, etc
-    struct canfd_frame frame;
-    memset(&frame, 0, sizeof(frame)); /* init CAN FD frame, e.g. LEN = 0 */
+    
+    struct pcanfd_msg pcan_msg;
+    pcan_msg.type = PCANFD_TYPE_CANFD_MSG;
+    pcan_msg.id = msg.id;
+    pcan_msg.data_len = msg.length;
+    memcpy(pcan_msg.data, msg.data, msg.length);
 
-    frame.can_id = msg.id;
-    frame.len = msg.length;// DLC_LUT[msg.length] << 16;
-    memcpy(frame.data, msg.data, msg.length);
-
-    if (write(socket_, &frame, sizeof(struct canfd_frame)) != sizeof(struct canfd_frame))
+    int err = pcanfd_send_msg(socket_, &pcan_msg);
+    if (err)
     {
-        perror("WRITE ERROR");
+        perror("pcanfd_send_msg() failed");
         return false;
     }
+
+
+    // // TODO: Make sure socket is open and can device is valid, etc
+    // struct canfd_frame frame;
+    // memset(&frame, 0, sizeof(frame)); /* init CAN FD frame, e.g. LEN = 0 */
+
+    // frame.can_id = msg.id;
+    // frame.len = msg.length;// DLC_LUT[msg.length] << 16;
+    // memcpy(frame.data, msg.data, msg.length);
+
+    // if (write(socket_, &frame, sizeof(struct canfd_frame)) != sizeof(struct canfd_frame))
+    // {
+    //     perror("WRITE ERROR");
+    //     return false;
+    // }
 
     // if (write(socket_, &frame, int(m_socket_mode)) != int(m_socket_mode)) {
     //         perror("write");
@@ -116,20 +139,35 @@ bool CANDevice::Send(CAN_msg_t &msg)
 bool CANDevice::Receive(CAN_msg_t &msg)
 {
     // TODO: Make sure socket is open and can device is valid, etc
-    struct canfd_frame frame;
+    // struct canfd_frame frame;
 
-    // Read in a CAN frame
-    auto num_bytes = read(socket_, &frame, CANFD_MTU);
-    //std::cout << "READ: " << num_bytes << std::endl;
-    if (num_bytes != CAN_MTU && num_bytes != CANFD_MTU)
+    // // Read in a CAN frame
+    // auto num_bytes = read(socket_, &frame, CANFD_MTU);
+    // //std::cout << "READ: " << num_bytes << std::endl;
+    // if (num_bytes != CAN_MTU && num_bytes != CANFD_MTU)
+    // {
+    //     //perror("READ ERROR");
+    //     return false;
+    // }
+
+    struct pcanfd_msg pcan_msg;
+    int err = pcanfd_recv_msg(socket_, &pcan_msg);
+    if (err)
     {
-        //perror("READ ERROR");
+        //perror("pcanfd_recv_msg() failed");
         return false;
     }
 
-    msg.id = frame.can_id;
-    msg.length = frame.len;
-    memcpy(msg.data, frame.data, frame.len);
+    if(pcan_msg.type != PCANFD_TYPE_CANFD_MSG || pcan_msg.id != 0x01)
+    {
+       // std::cout << "STATUS? " << std::endl;
+        return false;
+    }
+
+    //std::cout << "WHO TO: " << pcan_msg.id << std::endl;
+    msg.id = pcan_msg.id;
+    msg.length = pcan_msg.data_len;
+    memcpy(msg.data, pcan_msg.data, pcan_msg.data_len);
     return true;
 }
 
@@ -137,11 +175,12 @@ bool CANDevice::Close()
 {
     if (socket_ != -1)
     {
-        if (close(socket_) < 0)
-        {
-            perror("ERROR CLOSE");
-            return false;
-        }
+        pcanfd_close(socket_);
+        // if (close(socket_) < 0)
+        // {
+        //     perror("ERROR CLOSE");
+        //     return false;
+        // }
     }
     else
         return false;
